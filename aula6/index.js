37ccef23c2a4e9f63a5c240234e7f5798221c9d9
Mateus <mateusmtu@gmail.com>
2021-07-30 09:15:51 -0300

    aula 6 e 7

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+//sempre let pra variavel, 'var' é antigo ja
+let nome = 'João';
+let nomeCliente = "Armando";
+
+console.log("Variáveis");
+console.log("nome: " + nome);
+console.log("nome cliente: " + nomeCliente);
---
+++ aula7/index.js
@@ -0,0 +1,7 @@
+//sempre let pra variavel, 'var' é antigo ja
+const idade = 10;
+let nomeCliente = "Armando";
+
+console.log("Variáveis");
+console.log("nome: " + idade);
+console.log("nome cliente: " + nomeCliente);
